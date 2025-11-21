90fa4876aa53ea662c6d2f8beed29ba8b58a9340
SCNA052 <florian.schnaeckel@bertelsmann.de>
2025-11-21 20:15:36 +0100

    linked frontend

--- Backend/src/main/java/arvato/de/DeApplication.java
+++ Backend/src/main/java/arvato/de/DeApplication.java
@@ -3,11 +3,19 @@ package arvato.de;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class DeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DeApplication.class, args);
+
+        Thread.sleep(1000);
+
+        java.awt.Desktop.getDesktop().browse(new java.net.URI("http://localhost:8080"));
+
 	}
 
 }
